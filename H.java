cc31de97789212aa526ed7316845678c185fc9b8
yakanna <yakannakadem@gmail.com>
2025-04-09 22:23:58 +0530

    first

---
+++ H.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class H{
+    public static void main(String[] args){
+        System.out.println("yakanna");
+    }
+}
+// git init
+// git add README.md
+// git commit -m "first commit"
+// git branch -M main
+// git remote add origin https://github.com/yakanna2000/command.git
+// git push -u origin main
