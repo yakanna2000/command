f4ab11d69c6c205d877c7239e9059e4f10dca02f
Yakanna <167448285+yakanna2000@users.noreply.github.com>
2025-04-09 22:53:18 +0530

    Update H.java

--- H.java
+++ H.java
@@ -2,7 +2,7 @@ import java.util.*;
 
 public class H{
     public static void main(String[] args){
-        System.out.println("yakanna");
+        System.out.println("kanna");
     }
 }
 // git init
